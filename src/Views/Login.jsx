469264380bcf34261467051b8d2fbcf082e31029
Adnan Saleem <adnan13893@gmail.com>
2022-02-26 21:49:15 +0500

    added login/sign up views and routing

---
+++ src/Views/Login.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const LoginView = () => {
+  return <Header text="Log In" />
+}
+
+export default LoginView
---
+++ src/Views/Signup.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import Header from '../components/Header'
+
+export default function SignupView() {
+  return <Header text="Sign Up" />
+}
